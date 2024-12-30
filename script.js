3f6a3afd803f504372f3d3d7421ac80fe2fc387e
Xavierc1001hi <bob1001hi@gmail.com>
2024-12-31 12:06:26 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector(".button").addEventListener("click", function() {
-            window.location.href = ""; // 
+            window.location.href = "https://xavierc1001hi.github.io/penguin-photos/"; // 
         });
