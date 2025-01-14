d18cba8a0d7a6b319b5aaaa065baae34b85107c0
Xavierc1001hi <bob1001hi@gmail.com>
2025-01-14 18:04:49 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-document.querySelector(".button").addEventListener("click", function() {
-            window.location.href = "https://xavierc1001hi.github.io/penguin-photos/"; // 
-        });
+body {
+  background: #90D5FF;
+  color: #FFA500;
+  padding: 25px;
+  font-family: sans-serif;
+}
+.button {
+  background-color: #0000FF;
+  border: none;
+  color: white;
+  padding: 1px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+
+body {
+  background: #90D5FF;
+  color: #FFA500;
+  padding: 25px;
+  font-family: sans-serif;
+}
+.button {
+  background-color: #0000FF;
+  border: none;
+  color: white;
+  padding: 1px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+#map {
+  height: 100%;
+}
+
+/*
+ * Optional: Makes the sample page fill the window.
+ */
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
